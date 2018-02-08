77d74865e573dc627ae9b8f80c104fdb30ff9374
Nalrarang <nalrarang@gmail.com>
2018-02-08 18:42:55 +0900

    test

--- app.py
+++ app.py
@@ -15,32 +15,11 @@ def message():
     content = request.json['content']
     response = content
 
-
-    date = datetime.date.today()
-    today = date.strftime("%m/%d")
-
-
-    r = requests.get('http://zangsisi.net/')
-
-    plain_text = r.text
-
-    soup = BeautifulSoup(plain_text, 'html.parser')
-    recent_list = soup.select('#recent-manga')
-
-    text = ''
-    for selector in recent_list:
-        for a in selector.select('.list'):
-            for b in a.select('.date'):
-                if today == b.string:
-                    text += "★☆★☆" + today + " 만화 리스트★☆★☆\n"
-                    for c in a.select('.contents > a'):
-                        text += c.contents[0], c.contents[1].string + "\n"
-
     if response == 'ㅁㅎ' or response == '만화':
         return jsonify(
             {
                 'message': {
-                    'text': text
+                    'text': '만화'
                 },
                 'keyboard': {
                     'type': 'text'
@@ -60,6 +39,25 @@ def message():
 
             })
 
+
+def get_jangsisi_crawl():
+    date = datetime.date.today()
+    today = date.strftime("%m/%d")
+
+    r = requests.get('http://zangsisi.net/')
+
+    plain_text = r.text
+
+    soup = BeautifulSoup(plain_text, 'html.parser')
+    recent_list = soup.select('#recent-manga')
+
+    for selector in recent_list:
+        for a in selector.select('.list'):
+            for b in a.select('.date'):
+                if today == b.string:
+                    print "★☆★☆" + today + " 만화 리스트★☆★☆"
+                    for c in a.select('.contents > a'):
+                        print c.contents[0], c.contents[1].string
 
 
 if __name__ == '__main__':
